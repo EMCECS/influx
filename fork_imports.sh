8eb10b803ce396a89a63e4348f013712f47030ac
Andrey Kurilov <andrey.kurilov@emc.com>
2018-10-10 11:05:31 +0300

    update the fork imports script

--- fork_imports.sh
+++ fork_imports.sh
@@ -5,6 +5,7 @@ sed -i -e 's/\"github\.com\/influxdata\/platform\/query\/complete/\"github\.com\
 sed -i -e 's/\"github\.com\/influxdata\/platform\/query\/control/\"github\.com\/EMCECS\/flux\/control/g' $(find . | grep \\.go$)
 sed -i -e 's/\"github\.com\/influxdata\/platform\/query\/csv/\"github\.com\/EMCECS\/flux\/csv/g' $(find . | grep \\.go$)
 sed -i -e 's/\"github\.com\/influxdata\/platform\/query\/execute/\"github\.com\/EMCECS\/flux\/execute/g' $(find . | grep \\.go$)
+sed -i -e 's/\"github\.com\/influxdata\/platform\/query\/functions\/storage/\"github\.com\/EMCECS\/influx\/query\/functions\/storage/g' $(find . | grep \\.go$)
 sed -i -e 's/\"github\.com\/influxdata\/platform\/query\/functions/\"github\.com\/EMCECS\/flux\/functions/g' $(find . | grep \\.go$)
 sed -i -e 's/\"github\.com\/influxdata\/platform\/query\/parser/\"github\.com\/EMCECS\/flux\/parser/g' $(find . | grep \\.go$)
 sed -i -e 's/\"github\.com\/influxdata\/platform/\"github\.com\/EMCECS\/influx/g' $(find . | grep \\.go$)
