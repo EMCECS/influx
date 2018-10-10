cd91fc7e4356371670f1c60550916eea58065995
Andrey Kurilov <andrey.kurilov@emc.com>
2018-10-10 10:18:14 +0300

    put back the storage package

--- fork_imports.sh
+++ fork_imports.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-sed -i -e 's/\"github\.com\/influxdata\/platform\/query/\"github\.com\/EMCECS\/flux/g' $(find . | grep \\.go$)
 sed -i -e 's/\"github\.com\/influxdata\/platform/\"github\.com\/EMCECS\/influx/g' $(find . | grep \\.go$)
-sed -i -e 's/\"github.com\/EMCECS\/flux\/functions/storage\"github.com\/EMCECS\/flux\/functions\/inputs\/storage/g' $(find . | grep \\.go$)
