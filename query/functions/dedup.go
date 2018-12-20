a05ea7a849e8ab8af0ecd90955dfd4b4e1e95438
Kurilov, Andrey <andrey.kurilov@emc.com>
2018-12-20 17:07:50 +0300

    wip [skip ci]

--- query/functions/dedup.go
+++ query/functions/dedup.go
@@ -2,7 +2,6 @@ package functions
 
 import (
 	"fmt"
-
 	"github.com/EMCECS/influx/query"
 	"github.com/EMCECS/influx/query/execute"
 	"github.com/EMCECS/influx/query/plan"
@@ -99,80 +98,37 @@ func (t *dedupTransformation) Process(id execute.DatasetID, tbl query.Table) err
 
 	builder, created := t.cache.TableBuilder(tbl.Key())
 	if !created {
-		return fmt.Errorf("dedup found duplicate block with key: %v", tbl.Key())
+		return fmt.Errorf("dedup found duplicate table with key: %v", tbl.Key())
 	}
 	execute.AddTableCols(tbl, builder)
-
-	var (
-		lastBool   map[int]bool
-		lastInt    map[int]int64
-		lastUint   map[int]uint64
-		lastFloat  map[int]float64
-		lastString map[int]string
-		lastTime   map[int]execute.Time
-	)
-	lastBool = make(map[int]bool)
-	lastInt = make(map[int]int64)
-	lastUint = make(map[int]uint64)
-	lastFloat = make(map[int]float64)
-	lastString = make(map[int]string)
-	lastTime = make(map[int]execute.Time)
-
+	builderCols := builder.Cols()
+	colCount := len(builderCols)
+	colIdxTimestamp := -1
+	for i := 0; i < colCount; i ++ {
+		col := builderCols[i];
+		if "_time" == col.Label {
+			colIdxTimestamp = i
+		}
+	}
+	if colIdxTimestamp < 0 {
+		return fmt.Errorf("dedup: column _time not found in the table with key %v", tbl.Key())
+	}
+	uniqueTimestamps := make(map[execute.Time]struct{})
 	firstRow := true
 
 	return tbl.Do(func(cr query.ColReader) error {
-
 		l := cr.Len()
-		builderCols := builder.Cols()
-		colCount := len(builderCols)
-
 		// loop over the records
 		for i := 0; i < l; i ++ {
-
-			duplicateFlag := !firstRow // assume the row to be a duplicate if the row is not 1st
-
-			for j := 0; j < colCount; j ++ { // loop over the columns
-				col := builderCols[j]
-				switch col.Type {
-				case query.TBool:
-					if cr.Bools(j)[i] != lastBool[j] {
-						duplicateFlag = false
-					}
-					lastBool[j] = cr.Bools(j)[i]
-				case query.TInt:
-					if cr.Ints(j)[i] != lastInt[j] {
-						duplicateFlag = false
-					}
-					lastInt[j] = cr.Ints(j)[i]
-				case query.TUInt:
-					if cr.UInts(j)[i] != lastUint[j] {
-						duplicateFlag = false
-					}
-					lastUint[j] = cr.UInts(j)[i]
-				case query.TFloat:
-					if cr.Floats(j)[i] != lastFloat[j] {
-						duplicateFlag = false
-					}
-					lastFloat[j] = cr.Floats(j)[i]
-				case query.TString:
-					if cr.Strings(j)[i] != lastString[j] {
-						duplicateFlag = false
-					}
-					lastString[j] = cr.Strings(j)[i]
-				case query.TTime:
-					if cr.Times(j)[i] != lastTime[j] {
-						duplicateFlag = false
-					}
-					lastTime[j] = cr.Times(j)[i]
-				}
-			}
-
-			if !duplicateFlag {
-				execute.AppendRecord(i, cr, builder)
-			}
-
+			ts := cr.Times(colIdxTimestamp)[i]
 			if firstRow {
 				firstRow = false
+				uniqueTimestamps[ts] = struct{}{}
+			} else {
+				_, duplicateFlag := uniqueTimestamps[ts]
+				if !duplicateFlag {
+					execute.AppendRecord(i, cr, builder)
+				}
 			}
 		}
 		return nil
