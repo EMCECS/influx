ca6335d63dad2b93d3990477ac4e01fcb680a887
Nikolay Tikhonov <nikolai.tikhonov@gmail.com>
2018-12-21 19:57:19 +0300

    Revert "Slow dedup fix proposal"

--- query/functions/dedup_test.go
+++ query/functions/dedup_test.go
@@ -14,7 +14,8 @@ func TestDedup_PassThrough(t *testing.T) {
 		s := functions.NewDedupTransformation(
 			d,
 			c,
-			&functions.DedupProcedureSpec{},
+			&functions.DedupProcedureSpec{
+			},
 		)
 		return s
 	})
@@ -70,8 +71,8 @@ func TestDedup_Process(t *testing.T) {
 				Data: [][]interface{}{
 					{execute.Time(1), "a", 2.0},
 					{execute.Time(2), "a", 1.0},
-					{execute.Time(2), "a", 1.0},
 					{execute.Time(3), "b", 3.0},
+					{execute.Time(2), "a", 1.0},
 				},
 			}},
 			want: []*executetest.Table{{
@@ -115,6 +116,7 @@ func TestDedup_Process(t *testing.T) {
 					{execute.Time(1), "a", 2.0},
 					{execute.Time(2), "a", 1.0},
 					{execute.Time(3), "b", 3.0},
+					{execute.Time(3), "c", 1.0},
 				},
 			}},
 		},
